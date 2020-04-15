f6d8257e2d916f6bd0ba4af32d6390de26d8b06c
ruselcmplx <razinurov@gmail.com>
2020-04-15 12:35:16 +0300

    error handling

--- js/index.js
+++ js/index.js
@@ -5,13 +5,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/App';
 import Menu from './components/Menu';
 
-ReactDOM.render((
-   <BrowserRouter>
-      <div className="row justify-content-center">
-         <Switch>
-            <Route exact path="/menu" component={Menu} />
-            <Route component={App} />
-         </Switch>
-      </div>
-   </BrowserRouter>
-), document.getElementById('root'));
+const root = document.getElementById('root');
+
+if (root) {
+   ReactDOM.render((
+      <BrowserRouter>
+         <div className="row justify-content-center">
+            <Switch>
+               <Route exact path="/menu" component={Menu} />
+               <Route component={App} />
+            </Switch>
+         </div>
+      </BrowserRouter>
+   ), root);
+}
