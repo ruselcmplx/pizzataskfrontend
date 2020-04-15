a23aede791164d924784e1f7ed92d7874b4bb199
ruselcmplx <razinurov@gmail.com>
2020-04-15 10:13:14 +0300

    controllers setup

--- js/index.js
+++ js/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './components/App';
 import Menu from './components/Menu';
 
 ReactDOM.render((
    <BrowserRouter>
-      <div>
-         <nav className="container nav-container">
-            <ul className="nav mt-2 mb-2">
-               <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
-               </li>
-               <li className="nav-item">
-                  <Link className="nav-link" to="/menu">Menu</Link>
-               </li>
-            </ul>
-
-            <div className="mt-2 mb-2 nav-link">
-               Your order: {123}$
-            </div>
-         </nav>
-
+      <div className="row justify-content-center">
          <Switch>
             <Route exact path="/menu" component={Menu} />
             <Route component={App} />
--- views/register.blade.php
+++ views/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="root">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -26,12 +26,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
 
-                                @error('email')
+                                @error('phone')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
